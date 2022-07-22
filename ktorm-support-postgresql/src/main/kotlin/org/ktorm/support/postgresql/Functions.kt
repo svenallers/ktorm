323361d5b7fa22f66bcb1a64307ca0977b1b4ff3
Sven Allers <sven.allers@gmx.de>
2022-07-22 16:18:11 +0200

    Basic impl

---
+++ ktorm-support-postgresql/src/main/kotlin/org/ktorm/support/postgresql/Functions.kt
@@ -0,0 +1,34 @@
+package org.ktorm.support.postgresql
+
+import org.ktorm.dsl.cast
+import org.ktorm.expression.ArgumentExpression
+import org.ktorm.expression.FunctionExpression
+import org.ktorm.schema.*
+
+public fun <T : Enum<T>> arrayPosition(value: Array<T?>, column: ColumnDeclaring<T>): FunctionExpression<Int> {
+    val columnSqlType = column.sqlType
+    return if (columnSqlType is EnumSqlType<T> && columnSqlType.dataTypeName !=null) {
+        arrayPosition(value, column, columnSqlType.toEnumArraySqlType())
+    } else {
+        arrayPosition(value.map { it?.name }.toTypedArray(), column.cast(TextSqlType))
+    }
+}
+
+public inline fun <reified T : Enum<T>> arrayPosition(value: Collection<T?>, column: ColumnDeclaring<T>): FunctionExpression<Int> {
+    val columnSqlType = column.sqlType
+    return if (columnSqlType is EnumSqlType<T> && columnSqlType.dataTypeName !=null) {
+        arrayPosition(value.toTypedArray(), column, columnSqlType.toEnumArraySqlType())
+    } else {
+        arrayPosition(value.map { it?.name }.toTypedArray(), column.cast(TextSqlType))
+    }
+}
+
+public fun arrayPosition(value: TextArray, column: ColumnDeclaring<String>): FunctionExpression<Int> =
+    arrayPosition(value, column, TextArraySqlType)
+
+public fun <T : Any> arrayPosition(value: Array<T?>, column: ColumnDeclaring<T>, arraySqlType: SqlType<Array<T?>>): FunctionExpression<Int> =
+    FunctionExpression(
+        functionName = "array_position",
+        arguments = listOf(ArgumentExpression(value, arraySqlType), column.asExpression()),
+        sqlType = IntSqlType
+    )
